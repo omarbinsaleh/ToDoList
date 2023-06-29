3e12ae76de2b7fd45aa53c0f1d4123dc3564ce47
omarbinsaleh <omarbinsaleh44@gmail.com>
2023-06-29 19:42:32 +0400

    Update 2.00 - making the edit and delete button functioning is done

--- todolist.js
+++ todolist.js
@@ -59,6 +59,22 @@ document.addEventListener('DOMContentLoaded', () => {
         task_delete_el.innerHTML = "Delete"
         task_actions_el.appendChild(task_delete_el);
 
+        task_edit_el.addEventListener('click', () => {
+
+            if (task_edit_el.innerText.toLowerCase() == "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
+            }
+        });
+
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
+
         // Clear out the input field
         input.value = "";
 
